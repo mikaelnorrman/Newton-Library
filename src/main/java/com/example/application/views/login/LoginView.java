b23739acfa0c832e7c1dce436318bc946415d5f2
MikaelNorrman <mikael.norrman@gmail.com>
2021-05-09 09:27:19 +0200

    New login with LoginOverlay

--- src/main/java/com/example/application/views/login/LoginView.java
+++ src/main/java/com/example/application/views/login/LoginView.java
@@ -1,14 +1,11 @@
 package com.example.application.views.login;
 
 import com.example.application.data.service.AuthService;
+import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.textfield.PasswordField;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
@@ -18,25 +15,24 @@ import com.vaadin.flow.router.RouterLink;
 @PageTitle("Login")
 @CssImport("./styles/views/login/login-view.css")
 @RouteAlias(value = "") // Gör så man login sidan startar upp direkt
-public class LoginView extends Div {
+public class LoginView extends Composite<LoginOverlay> {
 
     public LoginView(AuthService authService) {
         setId("login-view");
-        var username = new TextField("Username");
-        var password = new PasswordField("Password");
-        add(
-                new H1("Welcome to Office Space"),
-                username,
-                password,
-                new Button("Login", event -> {
-                    try {
-                        authService.authenticate(username.getValue(), password.getValue());
-                        UI.getCurrent().navigate("home");
-                    } catch (AuthService.AuthException e) {
-                        Notification.show("Wrong credentials.");
-                    }
-                }),
-                 new RouterLink("Register", RegisterView.class)
-        );
+
+        LoginOverlay loginOverlay = getContent();
+        loginOverlay.setTitle("Welcome");
+        loginOverlay.setDescription("Manage your business tasks");
+        loginOverlay.setOpened(true);
+
+        loginOverlay.addLoginListener(event -> {try {
+            authService.authenticate(event.getUsername(), event.getPassword());
+            UI.getCurrent().navigate("home");
+        } catch (AuthService.AuthException e) {
+            e.printStackTrace();
+            Notification.show("Wrong credentials.");
+        }
+        });
+        new RouterLink("Register", RegisterView.class); // This part is not showing up???
     }
 }
