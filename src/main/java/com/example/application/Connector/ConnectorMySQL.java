5061f9335e2de01d1b7612a9a266f46f11227862
BitXplorer <70508247+BitXplorer@users.noreply.github.com>
2021-06-09 22:01:26 +0200

    Fixed Expired book button as well as fixed Success theme for loaned book button.

--- src/main/java/com/example/application/Connector/ConnectorMySQL.java
+++ src/main/java/com/example/application/Connector/ConnectorMySQL.java
@@ -66,4 +66,26 @@ public class ConnectorMySQL implements Connector {
         }
         return bool;
     }
+
+
+    public Boolean checkExpiredBook (Integer usersID, Integer bookID) throws SQLException {
+
+        Boolean bool = false;
+        int boolVal;
+        String sql = "Call checkExpiredBook(?,?,?);";
+        try (CallableStatement stmt = connection.prepareCall(sql)) {
+            stmt.setInt(1, usersID);
+            stmt.setInt(2, bookID);
+            stmt.registerOutParameter(3, Types.BOOLEAN);
+            stmt.execute();
+            boolVal = stmt.getInt(3);
+
+        }
+        if (boolVal == 0){
+            bool = false;
+        }else if (boolVal == 1){
+            bool = true;
+        }
+        return bool;
+    }
 }
